813730ddd0ed0ee1a3f8c8a29f7b3072eff9aff4
Asterisk@DESKTOP-5V275IP <tsheejmaus@yahoo.com>
2021-03-19 17:10:47 -0700

    testing check

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,10 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const Users = require("../users/users-model");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+router.post("/register", (req, res) => {
+  res.end("implement register, please!");
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -26,10 +29,30 @@ router.post('/register', (req, res) => {
     4- On FAILED registration due to the `username` being taken,
       the response body should include a string exactly as follows: "username taken".
   */
+  function isValid(user) {
+    return Boolean(
+      user.username && user.password && typeof user.password === "string"
+    );
+  }
+  if (isValid(req.body)) {
+    try {
+      const { username, password } = req.body;
+      const hash = bcrypt.hashSync(password, 14);
+      const user = { username, password: hash };
+      const addedUser = Users.add(user);
+      res.json(addedUser);
+    } catch (err) {
+      res.status(500).json({ message: "username taken" });
+    }
+  } else {
+    res.status(400).json({
+      message: "username and password required",
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post("/login", (req, res) => {
+  res.end("implement login, please!");
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -53,6 +76,45 @@ router.post('/login', (req, res) => {
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
+  const { username, password } = req.body;
+  function isValid(user) {
+    return Boolean(
+      user.username && user.password && typeof user.password === "string"
+    );
+  }
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = makeToken(user);
+          res.status(200).json({ message: "Welcome to our API", token });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "username and password required",
+    });
+  }
 });
+
+const { jwtSecret } = require("./secret.js");
+function makeToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role,
+    foo: "bar",
+  };
+  const options = {
+    expiresIn: "5 minutes",
+  };
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
